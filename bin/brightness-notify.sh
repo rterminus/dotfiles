af19ae5c995b7d8afa28c927701ce7584b51a816
rpterminal <luizrenanpontes@gmail.com>
2026-03-10 20:51:47 -0300

    add volume and brightness notifications when modified

--- bin/brightness-notify.sh
+++ bin/brightness-notify.sh
@@ -2,4 +2,4 @@
 
 brightness=$(brightnessctl info | grep -oP '\(\K[^%]+')
 
-dunstify -a "Brilho" -u low -h string:x-dunst-stack-tag:brightness -h int:value:"$brightness" -i display-brightness "Brilho: ${brightness}%"
+dunstify -a "brightness" -u low -h string:x-dunst-stack-tag:brightness -h int:value:"$brightness" -i display-brightness "brightness: ${brightness}%"
--- bin/volume-notify.sh
+++ bin/volume-notify.sh
@@ -4,7 +4,7 @@ volume=$(pamixer --get-volume)
 is_mute=$(pamixer --get-mute)
 
 if [ "$is_mute" == "true" ]; then
-    dunstify -a "Volume" -u low -h string:x-dunst-stack-tag:volume -i audio-volume-muted "Mutado"
+    dunstify -a "volume" -u low -h string:x-dunst-stack-tag:volume -i audio-volume-muted "muted"
 else
-    dunstify -a "Volume" -u low -h string:x-dunst-stack-tag:volume -h int:value:"$volume" -i audio-volume-high "Volume: ${volume}%"
+    dunstify -a "volume" -u low -h string:x-dunst-stack-tag:volume -h int:value:"$volume" -i audio-volume-high "volume: ${volume}%"
 fi
