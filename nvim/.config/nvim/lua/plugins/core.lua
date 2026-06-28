ecae484825fe16310af71f25a6518e5ed7c318e8
rterminus <luizrenanpontes@gmail.com>
2026-06-28 16:14:25 -0300

    conform adjusted

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -848,6 +848,7 @@ return {
 				html = { "prettierd", "prettier", stop_after_first = true },
 				css = { "prettierd", "prettier", stop_after_first = true },
 				astro = { "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
 				markdown = { "prettierd", "prettier", stop_after_first = true },
 			},
 		},
