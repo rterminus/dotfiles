ea3ee252670cac014f3b99b84608b6239b7268c3
rterminus <luizrenanpontes@gmail.com>
2026-08-25 15:00:00 -0300

    sync: backup on 2026-08-25 15:00

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,77 +1,11 @@
-require("relative-motions"):setup({
-	show_numbers = "relative",
-	show_unmatched_codes = true,
-	show_motion = true,
-	enter_mode = "first",
-})
+require("starship"):setup()
 
-require("yatline"):setup({
-	section_separator = { open = "", close = "" },
-	part_separator = { open = "", close = "" },
-	inverse_separator = { open = "", close = "" },
+Status:children_remove(3, Status.RIGHT)
 
-	padding = { inner = 1, outer = 1 },
-
-	style_a = {
-		bg = "white",
-		fg = "black",
-		bg_mode = {
-			normal = "white",
-			select = "brightyellow",
-			un_set = "brightred",
-		},
-	},
-	style_b = { bg = "brightblack", fg = "brightwhite" },
-	style_c = { bg = "black", fg = "brightwhite" },
-
-	permissions_t_fg = "green",
-	permissions_r_fg = "yellow",
-	permissions_w_fg = "red",
-	permissions_x_fg = "cyan",
-	permissions_s_fg = "white",
-
-	tab_width = 20,
-
-	selected = { icon = "󰻭", fg = "yellow" },
-	copied = { icon = "", fg = "green" },
-	cut = { icon = "", fg = "red" },
-
-	files = { icon = "", fg = "blue" },
-	filtereds = { icon = "", fg = "magenta" },
-
-	total = { icon = "󰮍 ", fg = "yellow" },
-	success = { icon = " ", fg = "green" },
-	failed = { icon = " ", fg = "red" },
-
-	show_background = false,
-	display_header_line = true,
-	display_status_line = false,
-
-	component_positions = { "header", "tab" },
-
-	header_line = {
-		left = {
-			section_a = { { type = "string", name = "tab_mode" } },
-			section_b = { { type = "line", name = "tabs" } },
-			section_c = {
-				{ type = "string", name = "hovered_path" },
-				{ type = "coloreds", name = "count" },
-			},
-		},
-		right = {
-			section_a = {
-				{ type = "string", name = "hovered_size" },
-				{ type = "string", name = "cursor_position" },
-			},
-			section_b = {
-				{ type = "string", name = "cursor_percentage" },
-			},
-			section_c = {
-				{ type = "string", name = "hovered_file_extension", params = { true } },
-				{ type = "coloreds", name = "permissions" },
-			},
-		},
-	},
-})
-
--- require("starship"):setup()
+function Status:render()
+	local left = self:children_render(self.LEFT)
+	local right = self:children_render(self.RIGHT)
+	return {
+		ui.Line({ left, ui.Span("  │  "), right }),
+	}
+end
