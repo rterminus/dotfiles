730824136f20e2d6c03d0fe13095dbc96ed39f77
rterminus <luizrenanpontes@gmail.com>
2026-08-25 16:00:00 -0300

    sync: backup on 2026-08-25 16:00

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,6 +1,16 @@
 require("starship"):setup()
 
-Status:children_remove(3, Status.RIGHT)
+-- require("auto-layout"):setup()
+
+require("yaziline"):setup({
+	separator_style = "empty",
+	color = "#888888",
+	secondary_color = "#1a1a1a",
+	default_files_color = "#4c4c4c",
+	selected_files_color = "#eeeeee",
+	yanked_files_color = "#D6D6D6",
+	cut_files_color = "#030303",
+})
 
 function Status:render()
 	local left = self:children_render(self.LEFT)
